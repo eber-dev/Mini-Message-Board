598587ba00d3d9ea49d14b9aff3a5d6fc55deb44
Eber <www.eberacunaquispe@gmail.com>
2026-08-13 17:19:14 -0500

    configuré los archivos estáticos de Express y entendí cómo conectar /static con la carpeta public usando express.static, __dirname y path.join

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,16 @@ import express from 'express';
 import fs from 'fs';
 import { Router } from 'express';
 import { añadir } from '../controller/addform.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static(path.join(__dirname, '../public')));
 
 app.set('view engine', 'ejs');
 app.set('views', '../views');
